448beca641376bc2589a54dac04b403035e93787
matsui-ayk <ayako.matsui.1228@gmail.com>
2026-04-20 18:27:44 +0900

    add header

--- laravel/resources/js/App.vue
+++ laravel/resources/js/App.vue
@@ -1,3 +1,9 @@
+<script setup>
+import Header from '@/components/Header.vue'
+</script>
+
 <template>
-  <router-view />
+    <Header />
+<!--    <router-view />-->
+<!--    ↑コメントアウトでwelcomeページを非表示にした-->
 </template>
